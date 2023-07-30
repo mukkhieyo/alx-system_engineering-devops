38868e8ca7b509c24728d2ec41219f268c754bf1
Mukhtar Audu <99723874+mukkhieyo@users.noreply.github.com>
2023-07-30 06:19:18 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Fix PHP file typo mistake in a LAMP wordpress setup
-
-exec {'correct typo':
-  provider => shell,
-  command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php"
-}
--- 0x17-web_stack_debugging_3/README.md
+++
@@ -1,3 +0,0 @@
-# Web stack debugging #3
-
-This project is about using Strace to debug and find out why Apache is returning a 500 error on wordpress.
